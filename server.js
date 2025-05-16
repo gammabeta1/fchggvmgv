88e3187a6e609efbab18b911377811eef4bf5ca7
gammabeta1 <tracybervelyn@gmail.com>
2025-05-15 23:47:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.post("/upload-id", upload.fields([{ name: "front" }, { name: "back" }]), asy
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
